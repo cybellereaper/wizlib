6c44eb7727e8bb12ca822bcbf291e8267179d9f4
6d6577 <79605349+6d6577@users.noreply.github.com>
2023-05-13 11:47:54 -0500

    Update kiosks.go

--- kiosks.go
+++ kiosks.go
@@ -6,17 +6,17 @@ import (
 )
 
 type Kiosk struct {
-	Id          string                          `bson:"user_id"`
-	Title       string                          `bson:"title"`
-	KioskItems  map[string]map[string]KioskItem `bson:"items"`
-	LastUpdated int64                           `bson:"last_updated"`
+	Id          string                          `json:"user_id"`
+	Title       string                          `json:"title"`
+	KioskItems  map[string]map[string]KioskItem `json:"items"`
+	LastUpdated int64                           `json:"last_updated"`
 }
 
 type KioskItem struct {
-	Image       string `bson:",omitempty"`
-	Stock       int64  `bson:"stock"`
-	Price       int64  `bson:"price"`
-	LastUpdated int64  `bson:"last_updated"`
+	Image       string `json:",omitempty"`
+	Stock       int64  `json:"stock"`
+	Price       int64  `json:"price"`
+	LastUpdated int64  `json:"last_updated"`
 }
 
 func (k *Kiosk) Add(name, itemType string, item KioskItem) {
