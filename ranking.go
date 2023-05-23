78fa6ccb9e6b9d614fd036e29fef02b3de762db2
Astridalia <79605349+Astridalia@users.noreply.github.com>
2023-05-23 08:38:46 -0500

    added in player ranking for https://www.wizard101.com/pvp/pvp-rankings?age=4,https://www.wizard101.com/pvp/tournament-rankings?age=4

--- ranking.go
+++ ranking.go
@@ -7,6 +7,7 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+// PlayerRanking represents the ranking information of a player.
 type PlayerRanking struct {
 	Position string `json:"position"`
 	Name     string `json:"name"`
@@ -16,7 +17,9 @@ type PlayerRanking struct {
 	Rating   string `json:"rating"`
 }
 
+// parseFromSelection extracts the player ranking information from a goquery.Selection.
 func (r *PlayerRanking) parseFromSelection(s *goquery.Selection) {
+	// Extract and store the ranking information from the selection
 	r.Position = strings.TrimSpace(s.Find("td:nth-child(1)").Text())
 	r.Name = strings.TrimSpace(s.Find("td:nth-child(2)").Text())
 	r.Level = strings.TrimSpace(s.Find("td:nth-child(3)").Text())
@@ -25,26 +28,39 @@ func (r *PlayerRanking) parseFromSelection(s *goquery.Selection) {
 	r.Rating = strings.TrimSpace(s.Find("td:nth-child(6)").Text())
 }
 
+// FetchRankings retrieves the player rankings from the specified URL.
 func FetchRankings(url string) ([]PlayerRanking, error) {
+	// Fetch the HTML content from the URL
 	doc, err := FetchHTML(url)
 	if err != nil {
 		return nil, err
 	}
+
+	// Parse and return the player rankings from the HTML document
 	return parseRankings(doc), nil
 }
 
+// parseRankings parses the player rankings from a goquery.Document.
 func parseRankings(doc *goquery.Document) []PlayerRanking {
 	rankings := make([]PlayerRanking, 0)
 	doc.Find(".schedule table tbody tr").Each(func(i int, s *goquery.Selection) {
+		// Create a new PlayerRanking instance
 		ranking := PlayerRanking{}
+
+		// Parse the ranking information from the selection and store it in the ranking instance
 		ranking.parseFromSelection(s)
+
+		// Append the ranking instance to the rankings slice
 		rankings = append(rankings, ranking)
 	})
+
 	return rankings
 }
 
+// PrintRankings prints the player rankings.
 func PrintRankings(rankings []PlayerRanking) {
 	for _, r := range rankings {
+		// Print the ranking information for each player
 		fmt.Printf("Position: %s\n", r.Position)
 		fmt.Printf("Name: %s\n", r.Name)
 		fmt.Printf("Level: %s\n", r.Level)
